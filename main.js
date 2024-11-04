9e84d54d846afbf67573e2fabbd35a547b341e58
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-11-04 16:40:24 +0530

    I changed my code and finalized my code

--- main.js
+++ main.js
@@ -7,45 +7,6 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');     // Toggles the active class for the navbar
 };
 
-/*============== Scroll section active link ===============*/
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        }
-    });
-
-    /*============== Sticky navbar ===============*/
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    /*============== Remove toggle icon and navbar ===============*/
-    menuIcon.classList.remove('fa-xmark');
-    navbar.classList.remove('active');
-};
-
-/*============== Scroll reveal ===============*/
-ScrollReveal({ 
-    distance: '80px',
-    duration: 2000,
-    delay: 200
-});
-
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .Projects-container, .Contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .home-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*============== Typed js ===============*/
 const typed = new Typed('.multiple-text', {
